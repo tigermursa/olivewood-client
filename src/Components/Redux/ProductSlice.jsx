5d0dc45c4ce115b44fd834592613e349110faf41
mursalin@gmail.com <MURSA>
2023-08-24 01:31:32 +0600

    created productSlice goint to link with the store

--- src/Components/Redux/ProductSlice.jsx
+++ src/Components/Redux/ProductSlice.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import { build } from "vite";
 
-
-
-
-
-const ProductSlice = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const initialState = {
+  data: [],
+  state: "idle",
 };
 
-export default ProductSlice;
+const ProductSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = "idle";
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.status = "error";
+      });
+  },
+});
+
+export const { fetchProducts } = ProductSlice.actions;
+export default ProductSlice.reducer;
+export const getProducts = createAsyncThunk("products/get", async () => {
+  const data = await fetch("/data.json");
+  const result = await data.json();
+  return result;
+});
